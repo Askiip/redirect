98168372ebacedbd1a3d74d890f52f98a4c01806
Askiip <alice.borie@edu.ece.fr>
2021-07-14 11:16:23 +0200

    front add

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.listen(port, ()=>{
     console.log("done");
 });
 
-app.get("/", (req, res)=>{
+/*app.get("/", (req, res)=>{
     res.send("arrivee sur la page :)");
-});
+});*/
 
 app.get("/locali", (req, res)=>{
     res.send("lat=49.234, long=2.123");
