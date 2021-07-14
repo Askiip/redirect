6bc95c6b0153ff2cab4defc14d4d3385b4495116
Askiip <alice.borie@edu.ece.fr>
2021-07-14 12:43:57 +0200

    test front

--- view/connection.js
+++ view/connection.js
@@ -2,7 +2,7 @@ const express = require('express')
 const viewFiles = express.Router()
 
 viewFiles.get('', async(req, res) => {
-  res.render('home')
+  res.render('home.ejs')
 })
 
 module.exports = viewFiles
